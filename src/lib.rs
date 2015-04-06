fe63d298f2a72ee352afd553ca32269e17cee9c3
Gulshan Singh <gulshan@umich.edu>
2015-04-05 21:38:01 -0400

    Updated grammar/ast

--- src/lib.rs
+++ src/lib.rs
@@ -9,21 +9,84 @@ peg_file! gdl("grammar.rustpeg");
 
 #[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
 pub struct Program {
-    sexprs: Vec<SExpr>
+    pub clauses: Vec<Clause>
 }
 
 #[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
-pub enum SExpr {
-    Func(String, Vec<Term>),
-    RuleOp(Vec<Term>)
+pub enum Clause {
+    RuleClause(Rule),
+    SentenceClause(Sentence)
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Rule {
+    head: Sentence,
+    body: Vec<Literal>
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub enum Sentence {
+    PropSentence(Proposition),
+    RelSentence(Relation)
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub enum Literal {
+    NotLit(Not),
+    OrLit(Or),
+    DistinctLit(Distinct),
+    PropLit(Proposition),
+    RelLit(Relation)
 }
 
 #[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
 pub enum Term {
-    Constant(String),
-    Variable(String),
-    ExprTerm(SExpr),
-    Num(u8)
+    VarTerm(Variable),
+    FuncTerm(Function),
+    ConstTerm(Constant)
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Proposition {
+    name: Constant
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Relation {
+    name: Constant,
+    args: Vec<Term>
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Not {
+    lit: Box<Literal>
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Or {
+    lits: Vec<Literal>
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Distinct {
+    term1: Term,
+    term2: Term
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Variable {
+    name: Constant
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Function {
+    name: Constant,
+    args: Vec<Term>
+}
+
+#[derive(Debug, Eq, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct Constant {
+    name: String
 }
 
 pub fn parse(s: &str) -> Program {
